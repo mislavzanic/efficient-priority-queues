a2db6d8f902cbd2fa1ece09c34f22d390ed422a4
Mislav Zanic <mislav.zanic@photomath.com>
2022-05-24 09:07:24 +0200

    progress...

--- main.go
+++ main.go
@@ -10,8 +10,7 @@ import (
 func main() {
 	heap := Brodal.NewEmptyHeap()
 	rand.Seed(time.Now().Unix())
-	for i := 0; i < 100000000; i++ {
-		// println(i)
+	for i := 0; i < 1000000; i++ {
 		val := rand.Float64()
 		heap.Insert(Brodal.ValType(val))
 	}
